bcb1d904f22141d01f022c189cda339acfa44feb
Opetit <goodies75@hotmail.com>
2019-10-20 13:24:14 +0200

    gulpfile modified passage scss css

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,17 @@
 ﻿var gulp = require('gulp');
+//JS
+var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/base.scss')
+        .pipe(sass()) // Using gulp-sass
+        .pipe(gulp.dest('app/css'))
+});
+//
+//gulp.watch('files-to-watch', ['tasks', 'to', 'run']); 
+//gulp.task('watch', function () {
+  //  gulp.watch('app/scss/**/*.scss', ['sass']);
+    // Other watchers
+//})
